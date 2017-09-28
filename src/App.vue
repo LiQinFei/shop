fcd4375f9a58271647b73ba280d08b4a896ab347
LiQf 李秦飞 <LiQf@seinclubank.com>
2017-09-28 17:38:03 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+   created() {
+    document.body.removeChild(document.getElementById('appLoading'))
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/reset.css";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
